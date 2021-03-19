c600f13347b8b7c5e97e22a835755cd847c8999f
huabingsen <huabingsen@qq.com>
2021-03-19 23:00:04 +0800

    菜鸟教程-正则-学到修饰符(将开始)

---
+++ 3-19/transformMoneyNumber.js
@@ -0,0 +1,10 @@
+function transformMoneyNumber(num) {
+  if (num === 0) {
+    let zero = 0;
+    return zero.toFixed(2);
+  }
+}
+
+// 未完待续...
+let moneyNumber = transformMoneyNumber(0);
+console.log(moneyNumber);
